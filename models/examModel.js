50d27a47daecf02a9697b1c41267ecba5281a0ea
Saurab Khatiwoda <saurabkhatiwoda@gmail.com>
2025-09-17 22:12:17 +0545

    Update examModel.js

--- models/examModel.js
+++ models/examModel.js
@@ -1,26 +1,24 @@
 //examModel.js
+// examModel.js
 import mongoose from 'mongoose';
 
 const examSchema = new mongoose.Schema({
-  examData: {
-    type: Object,
-    required: true,
-    setId: { type: Number, required: true },
-    title: { type: String, required: true },
-    duration: { type: Number, required: true },
-    sections: [{
-      name: { type: String, required: true },
-      questions: [{
-        id: { type: Number, required: true },
-        text: { type: String, required: true },
-        choices: [{
-          option: { type: String, required: true },
-          text: { type: String, required: true }
-        }],
-        answer: { type: String, required: true }
-      }]
+  // These are top-level fields in your DB, so define them as such
+  setId: { type: Number, required: true },
+  title: { type: String, required: true },
+  duration: { type: Number, required: true },
+  sections: [{
+    name: { type: String, required: true },
+    questions: [{
+      id: { type: Number, required: true },
+      text: { type: String, required: true },
+      choices: [{
+        option: { type: String, required: true },
+        text: { type: String, required: true }
+      }],
+      answer: { type: String, required: true }
     }]
-  },
+  }],
   createdAt: {
     type: Date,
     default: Date.now
